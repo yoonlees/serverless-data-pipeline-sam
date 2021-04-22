341b0a8e73851e74daf2d9864e5c7cef36e2b611
Yoon Lee <yoonlees@illinois.edu>
2021-04-21 22:57:39 -0500

    Push sqs queue to firehose

--- sqs-to-firehose-sam/lambda/app.py
+++ sqs-to-firehose-sam/lambda/app.py
@@ -1,54 +1,138 @@
-import json
 import boto3
+import traceback
+import json
 import os
-from datetime import datetime
+import uuid
+
+firehose_client = boto3.client('firehose')
+
+
+def extract_data(record):
+    '''
+    Description:
+    This function performs the data extraction from the record 
+    dictionary
+    :param record: [type: dict] corresponds to a message from a list 
+     of messages read by lambda
+    :return data: [type: str] extracted data
+    '''
+    # body = json.loads(record['body'])
+    body = record['body']
+    # data = body['Message']
+    # return data
+    return body
+
+
+def add_record(record, kinesis_records_all):
+    '''
+    Description: 
+    This function extracts the required data from the record 
+    dictionary and adds it to the list of records to be pushed to 
+    the respective mapped Kinesis Data Stream based on the 
+    eventSourceARN. 
+    If data extraction throws exception then the whole record 
+    dictionary is dumped as a string to the "non_conformed" key in 
+    kinesis_records_all; eventually to be dumped in DLQStream.
+    :param record: [type: dict] corresponds to a message from a 
+     list of messages read by lambda
+
+    :param kinesis_records_all: [type: dict] contains sqs queue 
+     arns as keys mapped to a dict containing list of records 
+     to be pushed and the name of the destination Kinesis Data Stream
+    :return kinesis_records_all: [type: dict] updated dict 
+     kinesis_records_all 
+    '''
+    # creating record_dict dictionary
+    record_dict = {}
+    try:
+        # source arn
+        sqs_source_arn = record['eventSourceARN']
+
+        # Retrieving required data from record for 'Data' key.
+        # Value for 'Data' key can be as per your requirement.
+        data = extract_data(record)
+        record_dict['Data'] = data
+        record_dict['PartitionKey'] = str(uuid.uuid1())
+
+        # get sqs name
+        sqs_name = sqs_source_arn.split(":")[-1]
+
+        # appending to respective sqs_key in
+        # kinesis_records_all dictionary
+        kinesis_records_all[sqs_source_arn]['Records']\
+            .append(record_dict)
+
+        # get eventid
+        # eventId = json.loads(data)["eventId"]
+        sensor = data["sensor"]
+        print(f"Successfully processed sensor : {sensor} "
+              f"from SQS : {sqs_name}")
+    except Exception as e:
+        # dumping the whole record as is to non_conformed list
+        record_dict['Data'] = json.dumps(record)
+        record_dict['PartitionKey'] = str(uuid.uuid1())
+
+        # log exceptions
+        # message = '\n'.join([str(e), str(traceback.print_exc()])
+        message = "Something is wrong"
+        print(message)
+    finally:
+        return kinesis_records_all
+
+
+def push_to_kinesis(source, source_dict):
+    '''
+    Description:
+    This function pushes the list of extracted records from a 
+    queue / non_conformed records to a particular Kinesis Data Stream
+
+    :param source: [type: str] SQS queue ARN
+    :param source_dict: [type: dict] contains a list of records and 
+     the stream name where the records are to be pushed to
+    '''
+    if source_dict['Records']:
+        for record in source_dict['Records']:
+            response = firehose_client.put_record(
+                DeliveryStreamName=source_dict['StreamName'],
+                Record={
+                    'Data': json.dumps(record['Data'])
+                }
+            )
+            records_pushed_message = ''
+            if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                pushed_count = ' '.join(['Pushed',
+                                         str(len(source_dict['Records'])), 'records'])
+                from_details = ' '.join(['from', source])
+                to_details = ' '.join(['to',
+                                       source_dict['StreamName'], 'stream'])
+                records_pushed_message = \
+                    ' '.join([pushed_count, to_details]) \
+                    if source == 'non_conformed' else ' '.\
+                    join([pushed_count, from_details, to_details])
+                message = \
+                    ' '.join(['\nResponse received from',
+                              source_dict['StreamName'],
+                              'with HTTPStatusCode',
+                              str(response["ResponseMetadata"]
+                                  ["HTTPStatusCode"]),
+                              ".", records_pushed_message])
+                print(message)
+
 
 def lambda_handler(event, context):
-    
-    now = datetime.now()
-    timestamp = now.strftime("%Y-%m-%d.%H:%M:%S")
-    
-    stats = {}
-    positive_uins = []
-    handled_messages = []
-    
-    # gets keys 
-   
-    sqs = boto3.resource("sqs")
-    queue_name = os.environ.get('QUEUE_NAME')
-    print (f'queue_name is: {queue_name}')
-    queue = sqs.get_queue_by_name(QueueName=queue_name)
-    
-    s3_obj_prefix = os.environ.get('S3_OBJ_PREFIX')
-    s3_bucket = os.environ.get('S3_BUCKET')
-    
-    found_messages = True
-    while len(handled_messages) < 1000 and found_messages:
-        found_messages = False
-        for message in queue.receive_messages(MaxNumberOfMessages=10, VisibilityTimeout=30):
-            found_messages = True
-            data = json.loads(message.body)
-            payload = json.loads(data.get('Message','{}'))
-            positive_uins.append(payload.get('correlation_id'))
-            handled_messages.append(message)
-        
-    print(f'Handled {len(handled_messages)} messages')   
-    print(f'Positive UINs: {positive_uins}')
-    
-    if len(positive_uins):
-        file_contents = '\n'.join(positive_uins) + '\n'
-        s3 = boto3.resource('s3')
-        obj = s3.Object(s3_bucket,f'{s3_obj_prefix}/positives_{timestamp}.csv')
-        obj.put(Body=file_contents)
-    
-    for message in handled_messages:
-        message.delete()
-    
-    stats['positive_uins'] = len(positive_uins)
-    stats['lambda_timestamp'] = timestamp
-    
-    # Poll messages from SQS
-    
-    print(json.dumps({ 'statusCode': 200, 'body': stats }))
-    
-    return 0
+    kinesis_records_all = {}
+    sqs_to_kinesis_mapping = {
+        "arn:aws:sqs:us-east-2:430229884637:sqs-to-firehose-StreamQueue-VBWFFR44OUZC":
+        "sam-lrs-data-pipeline-DeliveryStream-8M7sKQ88lQhF"
+    }
+    for sqs_key in sqs_to_kinesis_mapping.keys():
+        kinesis_records_all[sqs_key] = {'Records': [],
+                                        'StreamName': sqs_to_kinesis_mapping[sqs_key]}
+
+    for record in event['Records']:
+        kinesis_records_all = add_record(record, kinesis_records_all)
+
+    # pushing records retrieved from each sqs source to respective
+    # streams & non-conformed records to DLQ Stream
+    for source in kinesis_records_all.keys():
+        push_to_kinesis(source, kinesis_records_all[source])
