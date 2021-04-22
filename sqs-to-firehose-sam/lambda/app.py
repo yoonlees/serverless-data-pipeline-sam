2ad8b933feff0f879175eb083e650cb61e18176c
Yoon Lee <yoonlees@illinois.edu>
2021-04-22 17:32:43 -0500

    SQS to Firehose works

--- sqs-to-firehose-sam/lambda/app.py
+++ sqs-to-firehose-sam/lambda/app.py
@@ -10,9 +10,9 @@ firehose_client = boto3.client('firehose')
 def extract_data(record):
     '''
     Description:
-    This function performs the data extraction from the record 
+    This function performs the data extraction from the record
     dictionary
-    :param record: [type: dict] corresponds to a message from a list 
+    :param record: [type: dict] corresponds to a message from a list
      of messages read by lambda
     :return data: [type: str] extracted data
     '''
@@ -25,22 +25,22 @@ def extract_data(record):
 
 def add_record(record, kinesis_records_all):
     '''
-    Description: 
-    This function extracts the required data from the record 
-    dictionary and adds it to the list of records to be pushed to 
-    the respective mapped Kinesis Data Stream based on the 
-    eventSourceARN. 
-    If data extraction throws exception then the whole record 
-    dictionary is dumped as a string to the "non_conformed" key in 
+    Description:
+    This function extracts the required data from the record
+    dictionary and adds it to the list of records to be pushed to
+    the respective mapped Kinesis Data Stream based on the
+    eventSourceARN.
+    If data extraction throws exception then the whole record
+    dictionary is dumped as a string to the "non_conformed" key in
     kinesis_records_all; eventually to be dumped in DLQStream.
-    :param record: [type: dict] corresponds to a message from a 
+    :param record: [type: dict] corresponds to a message from a
      list of messages read by lambda
 
-    :param kinesis_records_all: [type: dict] contains sqs queue 
-     arns as keys mapped to a dict containing list of records 
+    :param kinesis_records_all: [type: dict] contains sqs queue
+     arns as keys mapped to a dict containing list of records
      to be pushed and the name of the destination Kinesis Data Stream
-    :return kinesis_records_all: [type: dict] updated dict 
-     kinesis_records_all 
+    :return kinesis_records_all: [type: dict] updated dict
+     kinesis_records_all
     '''
     # creating record_dict dictionary
     record_dict = {}
@@ -64,7 +64,8 @@ def add_record(record, kinesis_records_all):
 
         # get eventid
         # eventId = json.loads(data)["eventId"]
-        sensor = data["sensor"]
+        print(f'type of data: {type(data)}')
+        sensor = json.loads(data)["sensor"]
         print(f"Successfully processed sensor : {sensor} "
               f"from SQS : {sqs_name}")
     except Exception as e:
@@ -73,8 +74,8 @@ def add_record(record, kinesis_records_all):
         record_dict['PartitionKey'] = str(uuid.uuid1())
 
         # log exceptions
-        # message = '\n'.join([str(e), str(traceback.print_exc()])
-        message = "Something is wrong"
+        message = '\n'.join([str(e), str(traceback.print_exc())])
+        # message = "Something is wrong"
         print(message)
     finally:
         return kinesis_records_all
@@ -83,19 +84,21 @@ def add_record(record, kinesis_records_all):
 def push_to_kinesis(source, source_dict):
     '''
     Description:
-    This function pushes the list of extracted records from a 
+    This function pushes the list of extracted records from a
     queue / non_conformed records to a particular Kinesis Data Stream
 
     :param source: [type: str] SQS queue ARN
-    :param source_dict: [type: dict] contains a list of records and 
+    :param source_dict: [type: dict] contains a list of records and
      the stream name where the records are to be pushed to
     '''
     if source_dict['Records']:
         for record in source_dict['Records']:
+            print(f"recore data type: {type(record['Data'])}")
             response = firehose_client.put_record(
                 DeliveryStreamName=source_dict['StreamName'],
                 Record={
-                    'Data': json.dumps(record['Data'])
+                    # 'Data': json.dumps(record['Data'])
+                    'Data': record['Data']
                 }
             )
             records_pushed_message = ''
@@ -105,25 +108,24 @@ def push_to_kinesis(source, source_dict):
                 from_details = ' '.join(['from', source])
                 to_details = ' '.join(['to',
                                        source_dict['StreamName'], 'stream'])
-                records_pushed_message = \
-                    ' '.join([pushed_count, to_details]) \
+                records_pushed_message = ' '.join([pushed_count, to_details]) \
                     if source == 'non_conformed' else ' '.\
                     join([pushed_count, from_details, to_details])
-                message = \
-                    ' '.join(['\nResponse received from',
-                              source_dict['StreamName'],
-                              'with HTTPStatusCode',
-                              str(response["ResponseMetadata"]
-                                  ["HTTPStatusCode"]),
-                              ".", records_pushed_message])
+                message = ' '.join(['\nResponse received from',
+                                    source_dict['StreamName'],
+                                    'with HTTPStatusCode',
+                                    str(response["ResponseMetadata"]
+                                        ["HTTPStatusCode"]),
+                                    ".", records_pushed_message])
                 print(message)
 
 
 def lambda_handler(event, context):
     kinesis_records_all = {}
+    print(os.environ['QUEUE_NAME'])
     sqs_to_kinesis_mapping = {
-        "arn:aws:sqs:us-east-2:430229884637:sqs-to-firehose-StreamQueue-VBWFFR44OUZC":
-        "sam-lrs-data-pipeline-DeliveryStream-8M7sKQ88lQhF"
+        os.environ['QUEUE_NAME']:
+        os.environ['FIREHOSE_DELIVERY_STREAM_NAME']
     }
     for sqs_key in sqs_to_kinesis_mapping.keys():
         kinesis_records_all[sqs_key] = {'Records': [],
